0a4fd47620252f83bbf612ed2824fc45efb6fe16
Humenyuk94 <80436417+Humenyuk94@users.noreply.github.com>
2022-01-22 22:03:14 +0200

    Solve

--- Rest-api/dbconfig.js
+++ Rest-api/dbconfig.js
@@ -1,15 +1,19 @@
 var sql = require('mssql');
-const config = {
-    user :'UserTest123',
-    password :'Frostmorn23@',
-    server:'mssql-66932-0.cloudclusters.net',
-    database:'STO',
-    options:{
-        encrypt: true, // for azure
+var config = {  
+        server: 'mssql-66932-0.cloudclusters.net',  //update me
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'UserTest123', //update me
+                password: 'Frostmorn23@'  //update me
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: true,
+            database: 'STO'  //update me
+			 encrypt: true, // for azure
         trustServerCertificate: true,
         instancename :'mssql-66932-0.cloudclusters.net'
-    },
-    Port : 18209
-}
-
+        }
 module.exports = config; 
