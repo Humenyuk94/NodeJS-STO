ca8ff67b5fe99f34ae8201453c2786fd623f1644
Humenyuk94 <80436417+Humenyuk94@users.noreply.github.com>
2022-01-29 06:44:45 +0200

    Update dboperations.js

--- Rest-api/dboperations.js
+++ Rest-api/dboperations.js
@@ -6,200 +6,200 @@ var currentId = 0;
 const db = require('./settings/db')
 
 async function getAuths() {
-	    db.query("SELECT `AuthId`, `Mail`,`Pass` FROM `Auth`", (error, rows, fields)=>{
-			if(error) {
-				response.status(400, error, res)
-			} else {
-				response.status(200, rows, res)
-			}
-		})
-	    return auths.recordset;
+	db.query("SELECT `AuthId`, `Mail`,`Pass` FROM `Auth`", (error, rows, fields)=>{
+		if(error) {
+			response.status(400, error, res)
+		} else {
+			response.status(200, rows, res)
+		}
+	})
+	return auths.recordset;
 }
 
 async function getRecords() {
 	db.query("Select r.RecordId, s.Name, r.CreateDate, c.Name+' '+c.Surname+' '+c.Patronymic as 'Customer', r.PaymentType, w.Surname+' '+w.Name as 'Worker', r.RecordState from Record r join Serv s on s.ServId = r.ServId join Customer c on c.CustomerId = r.CustomerId join Worker w on w.WorkerId = r.WorkerId",(error, rows, fields)=>{
-				if(error) {
-				response.status(400, error, res)
-			} else {
-				response.status(200, rows, res)
-			}
-		})
-        console.log(records.recordset)
-		return records.recordset;
-    
+		if(error) {
+			response.status(400, error, res)
+		} else {
+			response.status(200, rows, res)
+		}
+	})
+	console.log(records.recordset)
+	return records.recordset;
+
 }
 
 async function getURecords() {
-    try {
-        let pool = await sql.createConnection(config);
+	try {
+		let pool = await sql.createConnection(config);
 		//console.log("curr "+currentId);
 		db.query("")
 		let customer = await pool
 			.input('authId', sql.Int, currentId)
 			.query("SELECT * from Customer where AuthId = @authId");
 		let customerId = customer.recordset[0].CustomerId;
-        let records = await pool
-		.input('customerId', sql.Int, customerId)
-		.query("Select r.RecordId, s.Name, r.CreateDate, c.Name+' '+c.Surname+' '+c.Patronymic as 'Customer', r.PaymentType, w.Surname+' '+w.Name as 'Worker', r.RecordState from Record r join Serv s on s.ServId = r.ServId join Customer c on c.CustomerId = r.CustomerId join Worker w on w.WorkerId = r.WorkerId where c.CustomerId = @customerId");
+		let records = await pool
+			.input('customerId', sql.Int, customerId)
+			.query("Select r.RecordId, s.Name, r.CreateDate, c.Name+' '+c.Surname+' '+c.Patronymic as 'Customer', r.PaymentType, w.Surname+' '+w.Name as 'Worker', r.RecordState from Record r join Serv s on s.ServId = r.ServId join Customer c on c.CustomerId = r.CustomerId join Worker w on w.WorkerId = r.WorkerId where c.CustomerId = @customerId");
 		//console.log(records)
-        return records.recordset;
-    }
-    catch (error) {
-        console.log(error);
-    }
+		return records.recordset;
+	}
+	catch (error) {
+		console.log(error);
+	}
 }
 
 async function getServ() {
-        db.query("SELECT * from Serv", (error, rows, fields)=>{
+	db.query("SELECT * from Serv", (error, rows, fields)=>{
 		if(error) {
-				response.status(400, error, res)
-			} else {
-				response.status(200, rows, res)
-			}
-		})
-        return serv.recordset;
-    }
+			response.status(400, error, res)
+		} else {
+			response.status(200, rows, res)
+		}
+	})
+	return serv.recordset;
+}
 
 async function getAuth(authId) {
-    try {
-        let pool = await sql.createConnection(config);
-        let auth = await pool
-            .input('input_parameter', sql.Int, authId)
-            .query("SELECT * from Auth where Id = @input_parameter");
-        return auth.recordsets;
+	try {
+		let pool = await sql.createConnection(config);
+		let auth = await pool
+			.input('input_parameter', sql.Int, authId)
+			.query("SELECT * from Auth where Id = @input_parameter");
+		return auth.recordsets;
 
-    }
-    catch (error) {
-        console.log(error);
-    }
+	}
+	catch (error) {
+		console.log(error);
+	}
 }
 async function getAuth(authId) {
-  db.query("SELECT * from Auth where Id = @input_parameter",=>{
-	    if(error) {
-            response.status(400, error, res)
-        }
-		else if(rows.length <= 0) {
-            response.status(401, {message: `Пользователь с email - ${@input_parameter} не найден. Пройдите регистрацию.`}, res)
-        }else {
-			
+	db.query("SELECT * from Auth where Id = @input_parameter",(error, rows, fields)=>{
+		if(error) {
+			response.status(400, error, res)
 		}
-		else {
-                    //Выкидываем ошибку что пароль не верный
-                    response.status(401, {message: `Пароль не верный.`}, res)
+		else if(rows.length <= 0) {
+			response.status(401, {message: `Пользователь с email - ${@input_parameter} не найден. Пройдите регистрацию.`}, res)
+		}else {
+			response.status(200, return auth.recordsets)
+		}
+	else {
+			//Выкидываем ошибку что пароль не верный
+			response.status(401, {message: `Пароль не верный.`}, res)
 
-                }
-  })
-            .input('input_parameter', sql.Int, authId)
-            .query("SELECT * from Auth where Id = @input_parameter");
-        return auth.recordsets;
+		}
+	})
+.input('input_parameter', sql.Int, authId)
+		.query("SELECT * from Auth where Id = @input_parameter");
+	return auth.recordsets;
 
-    }
-    catch (error) {
-        console.log(error);
-    }
+}
+catch (error) {
+	console.log(error);
+}
 }
 
 async function addRecord(auth, auths) {
-    try {
-        let pool = await sql.createConnection(config);
-			var today = new Date();
-			var Serv = await pool.request().query("SELECT * from Serv");
-			var date = today.getFullYear()+'-'+today.getMonth()+'-'+today.getDate();
-			var ServId = auth.ServId;
-			//console.log(ServId)
-			var PaymentType = auth.PaymentType;
-			var CustomerId;
-			//console.log(auths);
-			for(i=0;i<auths.length;i++){
-				
-				if(auths[i].Mail==currentUser){
-					CustomerId = auths[i].AuthId;
-					//console.log(auths[i].AuthId);
-					break;
-				}
+	try {
+		let pool = await sql.createConnection(config);
+		var today = new Date();
+		var Serv = await pool.request().query("SELECT * from Serv");
+		var date = today.getFullYear()+'-'+today.getMonth()+'-'+today.getDate();
+		var ServId = auth.ServId;
+		//console.log(ServId)
+		var PaymentType = auth.PaymentType;
+		var CustomerId;
+		//console.log(auths);
+		for(i=0;i<auths.length;i++){
+
+			if(auths[i].Mail==currentUser){
+				CustomerId = auths[i].AuthId;
+				//console.log(auths[i].AuthId);
+				break;
 			}
-			
-			
+		}
+
+
 		var insertAuth = await pool
-            .input('ServId', sql.Int, ServId)
-            .input('CreateDate', sql.Date, date)
+			.input('ServId', sql.Int, ServId)
+			.input('CreateDate', sql.Date, date)
 			.input('CustomerId', sql.Int, CustomerId)
 			.input('PaymentType', sql.NVarChar, PaymentType)
 			.query("Insert into Record(ServId, CreateDate, CustomerId, PaymentType, WorkerId, RecordState) values (@ServId, @CreateDate, @CustomerId, @PaymentType, 1, 'Рассматривается')");
-			return insertAuth.recordset;
-        
-    }
-    catch (err) {
-        console.log(err);
-    }
+		return insertAuth.recordset;
+
+	}
+	catch (err) {
+		console.log(err);
+	}
 
 }
 
 
 async function addAuth(auth, auths) {
-    try {
-        let pool = await sql.createConnection(config);
-        
-			var Mail = auth.Mail;
-			var Pass = auth.Pass;
-			var isInDB = false;
-			for(i=0;i<auths.length;i++){
-				if(auths[i].Mail==auth.Mail){
-					isInDB = true;
-					break;
-				}
+	try {
+		let pool = await sql.createConnection(config);
+
+		var Mail = auth.Mail;
+		var Pass = auth.Pass;
+		var isInDB = false;
+		for(i=0;i<auths.length;i++){
+			if(auths[i].Mail==auth.Mail){
+				isInDB = true;
+				break;
 			}
-		var insertAuth;	
+		}
+		var insertAuth;
 		if(!isInDB){
-		insertAuth = await pool
-            //.input('AuthId', sql.Int, auth.AuthId)
-            .input('Mail', sql.NVarChar, auth.Mail)
-            .input('Pass', sql.NVarChar, auth.Pass)
-            //.execute('InsertAuths');
-			//
-			
+			insertAuth = await pool
+				//.input('AuthId', sql.Int, auth.AuthId)
+				.input('Mail', sql.NVarChar, auth.Mail)
+				.input('Pass', sql.NVarChar, auth.Pass)
+				//.execute('InsertAuths');
+				//
+
 				.query("Insert into Auth (Mail, Pass) values (@Mail,@Pass)");
 			//}
 			return insertAuth.recordset;
 		} else {
 			return insertAuth.recordset;
 		}
-        
-    }
-    catch (err) {
-        console.log(err);
-    }
+
+	}
+	catch (err) {
+		console.log(err);
+	}
 
 }
 
 async function Authent(auth, auths) {
-    try {
+	try {
 		let pool = await sql.createConnection(config);
-		
-			var Mail = auth.Mail;
-			var Pass = auth.Pass;
-			var canLogin = false;
-			var isWorker = false;
-			for(i=0;i<auths.length;i++){
-				if(auths[i].Mail==auth.Mail && auths[i].Pass == auth.Pass){
-					let customer = await pool
+
+		var Mail = auth.Mail;
+		var Pass = auth.Pass;
+		var canLogin = false;
+		var isWorker = false;
+		for(i=0;i<auths.length;i++){
+			if(auths[i].Mail==auth.Mail && auths[i].Pass == auth.Pass){
+				let customer = await pool
 					.input('authId', sql.Int, auths[i].AuthId)
 					.query("SELECT * from Customer where AuthId = @authId");
-					if(customer.recordset[0].TypeAccess == 'Worker'){
-						console.log(customer.recordset[0].TypeAccess)
-						isWorker = true;
-					}
-					canLogin = true;
-					currentUser = auth.Mail;
-					currentId = customer.recordset[0].AuthId;
-					break;
+				if(customer.recordset[0].TypeAccess == 'Worker'){
+					console.log(customer.recordset[0].TypeAccess)
+					isWorker = true;
 				}
+				canLogin = true;
+				currentUser = auth.Mail;
+				currentId = customer.recordset[0].AuthId;
+				break;
 			}
-			return [canLogin, isWorker];
-    }
-    catch (err) {
-        console.log(err);
-    }
+		}
+		return [canLogin, isWorker];
+	}
+	catch (err) {
+		console.log(err);
+	}
 
 }
 
@@ -209,9 +209,9 @@ async function Authent(auth, auths) {
 
 
 module.exports = {
-    getAuths: getAuths,
-    getAuth : getAuth,
-    addAuth : addAuth,
+	getAuths: getAuths,
+	getAuth : getAuth,
+	addAuth : addAuth,
 	Authent : Authent,
 	getServ : getServ,
 	addRecord : addRecord,
